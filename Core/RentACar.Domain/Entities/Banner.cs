347b2d18808983345c53db401d9f0f86ac778a63
Egemen Gunduz <86930714+gndzegemen@users.noreply.github.com>
2024-06-06 15:12:16 +0300

    feat:  create structure and add entities

---
+++ Core/RentACar.Domain/Entities/Banner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Entities
+{
+    public class Banner
+    {
+        public int BannerID { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public int VideoDescription { get; set; }
+        public int VideoUrl { get; set; }
+
+    }
+}
---
+++ Core/RentACar.Domain/Entities/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public string Model {  get; set; }
+        public string CoverImageUrl {  get; set; }
+        public int Km {  get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+
+    }
+}
